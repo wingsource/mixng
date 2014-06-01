e35492964e5f2f7836a9b49dd7eb1fe053f92fc9
samikc <samikc@scoopspot.com>
2014-06-01 09:04:51 +0530

    Added one more controller

--- src/main/webapp/static/web/js/gadget1.js
+++ src/main/webapp/static/web/js/gadget1.js
@@ -8,4 +8,11 @@ angular.module("g1",[]).controller("Gadget1Controller",["$scope",function($scope
     $scope.salute = function() {
         console.log("Hello, "+$scope.name);
     }
+}]).controller("Gadget2Controller",["$scope",function($scope){
+    $scope.add = function() {
+        console.log($scope.name);
+    };
+    $scope.salute = function() {
+        console.log("Hello, "+$scope.name);
+    }
 }]);
